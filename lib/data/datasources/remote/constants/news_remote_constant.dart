7d0e68cc89a8a1f8cc9fe6d0927ebf3c996da9a9
gouresh ghadi <goureshghadi40@gmail.com>
2022-07-02 18:16:46 +0530

    remove api key

--- lib/data/datasources/remote/constants/news_remote_constant.dart
+++ lib/data/datasources/remote/constants/news_remote_constant.dart
@@ -3,5 +3,5 @@ class NewsEndPoints {
   static const String getHeadlinesNews =
       '/v2/top-headlines';
   static const String baseUrl = 'https://newsapi.org';
-  static const String newsApiKey = 'a7d26da747d042da9eea0d1ae1e47ce7';
+  static const String newsApiKey = '';
 }
